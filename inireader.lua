5bee2202b113ba916e2780ff46b0b1efc1125393
luluco250 <luluco250@gmail.com>
2017-02-08 23:15:21 -0200

    formatted script as a module

--- inireader.lua
+++ inireader.lua
@@ -1,91 +1,91 @@
 --[[
-	INI File Reader
-	
-	Returns a dictionary table separated 
-	in categories with names in them.
-	
-	dictionary:get(category, name, default)
-	Fetches values, returns 'default' if it
-	doesn't exist.
-	
-	dictionary?add(category, name, value)
-	Adds values to the dictionary, also
-	categories if necessary.
+	INI Reader by luluco250
 --]]
 
-local function clear(buf)
-	for i in pairs(buf) do
-		buf[i] = nil
+local inireader = {}
+
+inireader.map = { [""] = {} }
+
+local clear(buffer)
+	for i = 1, #buffer do
+		buffer[i] = nil
 	end
 end
 
-local file = io.open(...)
+function inireader.del(this)
+	for i = 1, #this.map do
+		this[i] = nil
+	end
+end
 
-local dictionary = {
-	add = function(t,c,n,v)
-		if (t[c] == nil) then
-			t[c] = {}
-		end
-		t[c][n] = v
-	end,
-	get = function(t,c,n,d)
-		if (t[c] == nil or t[c][n] == nil) then
-			return d
-		else
-			return t[c][n]
-		end
-	end,
-	[""] = {}
-}
+function inireader.add(this, category, name, value)
+	if (this[category] == nil) then
+		this[category] = {}
+	end
+	this[category][name] = value
+end
 
-local buffer = {""}
-local category = ""
-local name = ""
-local value = ""
+function inireader.get(this, category, name, default)
+	if (this[category] == nil or this[category][name] == nil) then
+		return default
+	else
+		return this[category][name]
+	end
+end
 
-for line in file:lines() do
-	if (line:sub(1,1) == '[') then
-		--build category
-		for i = 2, #line do
-			local c = line:sub(i,i)
-			if (c == ']') then
-				--fill 'category' with the
-				--contents of buffer, clear it
-				--and exit the loop
-				category = table.concat(buffer)
-				clear(buffer)
-				break
-			else
-				table.insert(buffer, c)
-			end
-		end
-	elseif not (line:sub(1,1) == ';' or line:sub(1,1) == '#') then
-		for i = 1, #line do
-			--each character in the current line
-			local c = line:sub(i,i)
-			--fill 'name' with the buffer,
-			--clear it and move to the next character
-			if (c == '=') then
-				name = table.concat(buffer)
-				clear(buffer)
-			else
-				table.insert(buffer, c)
-				--if we're on the last character,
-				--fill 'value' with the buffer,
-				--clear it and break the loop
-				if (i == #line) then
-					value = table.concat(buffer)
+function inireader.new(this, path)
+	local file = io.open(path)
+	
+	local buffer = {""}
+	
+	local category = ""
+	local name = ""
+	local value = ""
+	
+	for line in file:lines() do
+		if (line:sub(1,1) == '[') then
+			--build category
+			for i = 2, #line do
+				local c = line:sub(i,i)
+				if (c == ']') then
+					--fill 'category' with the
+					--contents of buffer, clear it
+					--and exit the loop
+					category = table.concat(buffer)
+					clear(buffer)
 					break
+				else
+					table.insert(buffer, c)
 				end
 			end
+		elseif not (line:sub(1,1) == ';' or line:sub(1,1) == '#') then
+			for i = 1, #line do
+				--each character in the current line
+				local c = line:sub(i,i)
+				--fill 'name' with the buffer,
+				--clear it and move to the next character
+				if (c == '=') then
+					name = table.concat(buffer)
+					clear(buffer)
+				else
+					table.insert(buffer, c)
+					--if we're on the last character,
+					--fill 'value' with the buffer,
+					--clear it and break the loop
+					if (i == #line) then
+						value = table.concat(buffer)
+						break
+					end
+				end
+			end
+			clear(buffer)
+			--each line in the file
+			this:add(category, name, value)
 		end
-		clear(buffer)
-		--each line in the file
-		dictionary:add(category, name, value)
 	end
+	
+	file:close()
+	
 end
 
-file:close()
-
-return dictionary
-
+return inireader
